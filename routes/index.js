1e56f9781a8716a44b8539ce55bf8533f458f512
Kiattisak Khamchaitep <tom131320131320@gmail.com>
2019-07-13 16:08:34 +0700

    link

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var connect = require("../config/database")
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'โครตโง่' });
+  var query = "select * from positions";
+  connect.query(query, function(error,result){
+    res.json(result)
+  })
+  
 });
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'โครตโง่' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'โครตโง่' });
 });
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'โครตโง่' });
